d13ea26e457d1a7ddda5d06c14e33b265037ae1c
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-02-07 22:28:26 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 TOKEN = "1084806593:AAGT--3sEIxYyu1NnS9RlFO9u2y3zoFSaBg"
 
-SUDOERS = [200097591, 945971280]
-TRD_CHAT = "@username" do canal ou id
-API_ID = SUA_API_ID
-API_HASH = "SUA_API_HASH"
+SUDOERS = [684842099, 684842099]
+TRD_CHAT = "@ElRanking" do canal ou id
+API_ID = "1117452" 
+API_HASH = "20a045b3dd24d6f7821e1758b170e866"
 
 VERSION = open("version.txt").read().rstrip()
