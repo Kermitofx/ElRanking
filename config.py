1f19e1e5a342869f2490e30f044b02d7116c54de
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-02-21 09:38:22 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 TOKEN = "726013526:AAGfT1sNYoF4Oh2rsv_TwJ7T92e2nqaUhgg"
 
 SUDOERS = [644110933]
-TRD_CHAT = "@ElRanking" do canal ou id
+TRD_CHAT = "1474627526" do canal ou id
 API_ID = "1117452" 
 API_HASH = "20a045b3dd24d6f7821e1758b170e866"
 
